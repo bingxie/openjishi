bb8d124c38dc152af7d2e06aac0efe4285577bba
Bing Xie <bing708@gmail.com>
2015-11-19 17:03:06 +1100

    Rubocop fix

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,35 +1,37 @@
-class Users::ConfirmationsController < Devise::ConfirmationsController
-  # GET /resource/confirmation/new
-  # def new
-  #   super
-  # end
+module Users
+  class ConfirmationsController < Devise::ConfirmationsController
+    # GET /resource/confirmation/new
+    # def new
+    #   super
+    # end
 
-  # POST /resource/confirmation
-  def create
-    super do |resource|
-      if successfully_sent?(resource)
-        respond_to do |format|
-          format.js {}
-          return
+    # POST /resource/confirmation
+    def create
+      super do |resource|
+        if successfully_sent?(resource)
+          respond_to do |format|
+            format.js {}
+            return
+          end
         end
       end
     end
+
+    # GET /resource/confirmation?confirmation_token=abcdef
+    # def show
+    #   super
+    # end
+
+    # protected
+
+    # The path used after resending confirmation instructions.
+    # def after_resending_confirmation_instructions_path_for(resource_name)
+    #   super(resource_name)
+    # end
+
+    # The path used after confirmation.
+    # def after_confirmation_path_for(resource_name, resource)
+    #   super(resource_name, resource)
+    # end
   end
-
-  # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
-
-  # protected
-
-  # The path used after resending confirmation instructions.
-  # def after_resending_confirmation_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
-
-  # The path used after confirmation.
-  # def after_confirmation_path_for(resource_name, resource)
-  #   super(resource_name, resource)
-  # end
 end
