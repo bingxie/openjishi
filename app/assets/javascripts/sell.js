df18d287e754210b5fddea9e240bf0c2848d04c9
Alex Zhen <suncrimson@gmail.com>
2015-11-19 15:42:19 +1100

    fixed login page bug and updated select style sheets

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -17,4 +17,7 @@ $().ready(function(){
   $("#select-sub-category").select2();
   $("#select-brand").select2();
   $("#select-condition").select2();
+  $("#province").select2();
+  $("#city").select2();
+  $("#select-2-demo,#select-2-1,#select-2-2,#select-2-3").select2();
 })
