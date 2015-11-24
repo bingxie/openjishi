bd5443dcaccbb42606c5e1a2d2f4b112c800d9d0
Bing Xie <bing708@gmail.com>
2015-11-24 13:16:23 +1100

    Test find and reset password

--- test/features/user_authentication_test.rb
+++ test/features/user_authentication_test.rb
@@ -3,11 +3,10 @@ require 'test_helper'
 class UserAuthenticationTest < ActionDispatch::IntegrationTest
   setup do
     ActionMailer::Base.deliveries.clear
+    Capybara.reset_sessions!
   end
 
   test 'user can sign up with email and password' do
-    Capybara.reset_sessions!
-
     visit new_user_registration_path
 
     assert_content "注册帐号"
@@ -39,6 +38,45 @@ class UserAuthenticationTest < ActionDispatch::IntegrationTest
       fill_in 'user[password]', with: '12345678'
 
       click_button '登录'
+    end
+
+    assert_content user.name
+
+    assert_equal root_path, page.current_path
+  end
+
+  test 'user can find password by email address' do
+    Capybara.current_driver = :poltergeist
+    user = users(:bing)
+
+    visit new_user_password_path
+
+    assert_content '找回密码'
+
+    within '#new_user' do
+      fill_in 'user[email]', with: user.email
+
+      click_button '发送重设密码链接到我邮箱'
+    end
+
+    wait_for_ajax
+
+    reset_email = ActionMailer::Base.deliveries.last
+
+    assert_equal ["support@guangchuan.com"], reset_email.from
+    assert_equal [user.email], reset_email.to
+    assert_equal '重置密码信息', reset_email.subject
+    assert_match /Change my password/, reset_email.body.to_s
+
+    visit URI.extract(reset_email.body.to_s).first
+
+    assert_content '重设密码'
+
+    within '#reset_password' do
+      fill_in 'user[password]', with: '87654321'
+      fill_in 'user[password_confirmation]', with: '87654321'
+
+      click_button '确定'
     end
 
     assert_content user.name
