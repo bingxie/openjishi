7993165c68fbb2475325d1d0fe219b92ec0af1b7
Bing Xie <bing708@gmail.com>
2015-12-01 10:18:05 +1100

    Generate product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130051947) do
+ActiveRecord::Schema.define(version: 20151130230611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,21 @@ ActiveRecord::Schema.define(version: 20151130051947) do
     t.datetime "updated_at"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string   "title",                     null: false
+    t.decimal  "price",       default: 0.0, null: false
+    t.string   "quality"
+    t.text     "description",               null: false
+    t.string   "taobao_url"
+    t.integer  "category_id"
+    t.integer  "brand_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "products", ["brand_id"], name: "index_products_on_brand_id", using: :btree
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "city_id"
@@ -120,5 +135,7 @@ ActiveRecord::Schema.define(version: 20151130051947) do
 
   add_foreign_key "brand_categories", "brands"
   add_foreign_key "brand_categories", "categories"
+  add_foreign_key "products", "brands"
+  add_foreign_key "products", "categories"
   add_foreign_key "profiles", "users"
 end
