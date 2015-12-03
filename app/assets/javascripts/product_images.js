d508f44130be4bd14ae0de65aa1f9ec8e7d9f310
Bing Xie <bing708@gmail.com>
2015-12-03 14:40:47 +1100

    Try dragzone api and configuration

--- app/assets/javascripts/product_images.js
+++ app/assets/javascripts/product_images.js
@@ -5,14 +5,22 @@ $(document).ready(function(){
   if ($('.dropzone').length) {
 
     var dropzone = new Dropzone (".dropzone", {
-      maxFilesize: 256, // set the maximum file size to 256 MB
+      maxFilesize: 2, // set the maximum file size to 2 MB
       paramName: "product_image[image]", // Rails expects the file upload to be something like model[field_name]
-      addRemoveLinks: true // don't show remove links on dropzone itself.
+      addRemoveLinks: true, // fales: don't show remove links on dropzone itself.
+      maxFiles: 3,
+      forceFallback: false, // Open for test
+      dictDefaultMessage: '拖拽图片到这里',
+      dictRemoveFile: '删除',
     });
 
     dropzone.on("success", function(file) {
-      this.removeFile(file);
-      $.getScript("/product_images");
-    })
+      // this.removeFile(file);
+      // $.getScript("/product_images");  # refresh upload file list
+    });
+
+    dropzone.on("removedfile", function(file) {
+      console.log('file removed');
+    });
   }
 });
