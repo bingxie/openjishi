3ea680241b77223f8f7bf233b60f68ecc7802672
Bing Xie <bing708@gmail.com>
2015-12-04 14:44:16 +1100

    Set form token when upload product image

--- app/controllers/product_images_controller.rb
+++ app/controllers/product_images_controller.rb
@@ -5,13 +5,15 @@ class ProductImagesController < ApplicationController
   end
 
   def create
-    @image = ProductImage.new(image_params)
-    @image.user_id = current_user.id
+    image = ProductImage.new(image_params)
+    image.user_id = current_user.id
+    image.form_token = params[:authenticity_token]
 
-    if @image.save
-      render json: { message: "success", fileID: @image.id }, status: 200
+
+    if image.save
+      render json: { message: "success", fileID: image.id }, status: 200
     else
-      render json: { error: @image.errors.full_messages.join(',')}, status: 400
+      render json: { error: image.errors.full_messages.join(',')}, status: 400
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203222944) do
+ActiveRecord::Schema.define(version: 20151204033247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20151203222944) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "user_id",            null: false
+    t.string   "form_token"
   end
 
   add_index "product_images", ["product_id"], name: "index_product_images_on_product_id", using: :btree
