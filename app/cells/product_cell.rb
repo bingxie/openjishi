2406f9cd3475712d47e6ddd4ea7c7ff723ffde5f
Bing Xie <bing708@gmail.com>
2015-12-07 15:02:43 +1100

    Split the product page into cells

--- app/cells/product_cell.rb
+++ app/cells/product_cell.rb
@@ -1,8 +1,37 @@
 class ProductCell < Cell::ViewModel
+  include Cell::Slim
+
   property :description
 
-  def show
+  def breadcrumbs
     render
   end
 
+  def preview_notice
+    if model.preview?
+      render 'preview_notice'
+    elsif model.published?
+      render 'publish_success'
+    end
+  end
+
+  def title_line
+    render
+  end
+
+  def image_panel
+    render
+  end
+
+  def info_panel
+    render
+  end
+
+  def description_panel
+    render
+  end
+
+  def seller_info
+    render
+  end
 end
