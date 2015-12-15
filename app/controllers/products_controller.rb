6444c5a6a686cf289c27cc59ba44011153a9734b
Bing Xie <bing708@gmail.com>
2015-12-15 21:26:53 +1100

    Add product controller test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :preview, :publish]
 
   before_action :set_product, only: [:show, :preview]
 
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -10,5 +10,27 @@ class ProductsControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+    assert_not_nil assigns(:product_form)
+  end
+
+  test 'preview should redirect to show when product is published' do
+    product = products(:one)
+    product.preview!
+    product.publish!
+
+    get :preview, id: product
+
+    assert_redirected_to product_path(product)
+  end
+
+  test 'should publish the product' do
+    product = products(:one)
+    product.preview!
+
+    put :publish, id: product
+
+    assert product.reload.published?
+
+    assert_redirected_to product_path(product)
   end
 end
